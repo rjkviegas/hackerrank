3920fc3463a92f661347dfed64f7d3a3ebe1998a
Ryan Viegas <rjkviegas@gmail.com>
2020-08-11 18:23:57 +0100

    passing test returns 12:00:00 when passed 12:00:00PM

--- time-conversion/lib/time_conversion.rb
+++ time-conversion/lib/time_conversion.rb
@@ -1,3 +1,7 @@
 def time_conversion(string)
-  "00:00:00"
+  if string[-2] == "A"
+    "00:00:00"
+  else
+    "12:00:00"
+  end
 end
--- time-conversion/spec/time_conversion_spec.rb
+++ time-conversion/spec/time_conversion_spec.rb
@@ -4,4 +4,7 @@ RSpec.describe 'time_conversion' do
   it "returns 00:00:00 when passed 12:00:00AM" do
     expect(time_conversion("12:00:00AM")).to eq("00:00:00")
   end
+  it "returns 12:00:00 when passed 12:00:00PM" do
+    expect(time_conversion("12:00:00PM")).to eq("12:00:00")
+  end
 end
